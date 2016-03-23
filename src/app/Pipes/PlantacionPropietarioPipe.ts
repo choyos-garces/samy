d4434581e48c3b5102e8e33a9d3688d201658395
Christopher Hoyos <choyos.garces@gmail.com>
2016-03-23 17:40:53 -0500

    add

---
+++ src/app/Pipes/PlantacionPropietarioPipe.ts
@@ -0,0 +1,12 @@
+import {Pipe} from "angular2/core";
+import {EmpresaModel} from "../Model/Administracion/EmpresaModel";
+import {PlantacionModel} from "../Model/Administracion/PlantacionModel";
+
+@Pipe({
+    name : 'plantacionPropietario'
+})
+export class PlantacionPropietarioPipe {
+    transform(collection :  PlantacionModel[], arg : EmpresaModel[]) : any[] {
+        return (typeof collection != "undefined" ) ? collection.filter((plantacion : PlantacionModel) => plantacion.propietario.id == arg[0].id ) : collection;
+    }
+}
